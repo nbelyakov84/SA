f1d2b990e3e3ed58b3f91ce07a25a9467cfb7b84
Nikolay <48524427+nbelyakov84@users.noreply.github.com>
2023-05-22 16:12:08 +0300

    Add files via upload

---
+++ Greenatom/_astra linux/astra init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the system time is accurate
+if [ $(date +%s) -lt 1577836800 ]; then
+    # Update system time
+    sudo timedatectl set-timezone Europe/Moscow
+    sudo ntpdate -s time.nist.gov
+fi
+
+echo System time was updated
+
+# Update apt repositories
+sudo apt update && apt upgrade -y
+
+echo All packages was updated
+
+# Open /etc/hostname in nano
+sudo nano /etc/hostname
+
+exit 0
