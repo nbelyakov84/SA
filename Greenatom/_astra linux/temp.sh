45d587440a602a398655d59162208a3770c2246e
suncatcher88 <nbelyakov84@yandex.ru>
2023-05-22 17:03:13 +0300

    added temp.sh

---
+++ Greenatom/_astra linux/temp.sh
@@ -0,0 +1,6 @@
+total 24
+14774051 drwxr-xr-x@ 5 nikolaybelyakov  staff   160 May 22 17:02 .
+13851848 drwxr-xr-x  5 nikolaybelyakov  staff   160 May 22 16:23 ..
+14774052 -rw-r--r--@ 1 nikolaybelyakov  staff  6148 May 22 16:41 .DS_Store
+14774053 -rw-r--r--@ 1 nikolaybelyakov  staff   445 May 22 16:37 astra init.sh
+14778814 -rw-r--r--  1 nikolaybelyakov  staff     0 May 22 17:02 temp.sh
